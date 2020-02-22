6abab7cb9cb85e3b859c57b6acdee5b02d26081c
Volodymyr Kheroim <volodymyr_kh@hotmail.com>
2020-02-22 16:19:13 +0200

    reloadInt commit

--- src/js/core.js
+++ src/js/core.js
@@ -8,7 +8,7 @@ import refs from './refs';
 import myPnotify from './pnotifyAlerts';
 import proxyElement from './proxyElemen';
 //---------------------------------------------------
-import user1 from './users/user1';
+import user1 from './users/reloadInt';
 import user2 from './users/user2';
 import user3 from './users/user3';
 import api from './api';
@@ -25,17 +25,13 @@ user1.test();
 // user2.test();
 // user3.test();
 
-<<<<<<< HEAD
+
 // api.getPopularFilms().then(console.log);
 // api.getInfoById(419704).then(console.log);
-// api.getMoviesByQuery('batman').then(console.log);
-=======
-api.getPopularFilms().then(console.log);
-api.getInfoById(419704).then(console.log);
 
 // api.getMoviesByQuery('batman').then(console.log);
 // api.page = 2;
 // api.getMoviesByQuery('batman').then(console.log);
 // console.log(api.getPageFromLink());
 // console.log(api.getMovieIdFromLink());
->>>>>>> 8e4b177e9c8738d00f5a8aa2fb37bf26690baec6
+
---
+++ src/js/users/reloadInt.js
@@ -0,0 +1,73 @@
+import refs from '../refs';
+import cardTemplate from '../../template/card.hbs';
+import mainPageTemplate from '../../template/main-page.hbs';
+import api from '../api';
+
+const user1 = {
+  mainPage() {
+    api.getPopularFilms().then(data => {
+      data.results.map(
+        elem => (elem.release_date = elem.release_date.split('-')[0]),
+      );
+      // console.log(data.results);
+      const markup = mainPageTemplate(data.results);    
+      insertCardsToMainPage(markup);
+      // console.log(data.results[0].release_date.split('-')[0]);
+    });
+  },
+  card(id) {
+    api.getInfoById(id).then(data => {
+      // console.log(data);
+      const markup = buildCartTemplate(data);
+      // console.log(markup);
+      insertCardToMain(markup);
+    });
+  },
+  buildCartTemplate(item) {
+    return cardTemplate(item);
+  },
+  insertCardToMain(item) {
+    refs.container.innerHTML = item;
+  },
+  insertCardsToMainPage(items) {
+    refs.cardList.insertAdjacentHTML('beforeend', items);
+  }
+};
+
+//card(419704);
+// mainPage();
+
+// function mainPage() {
+//   api.getPopularFilms().then(data => {
+//     data.results.map(
+//       elem => (elem.release_date = elem.release_date.split('-')[0]),
+//     );
+//     // console.log(data.results);
+//     const markup = mainPageTemplate(data.results);    
+//     insertCardsToMainPage(markup);
+//     // console.log(data.results[0].release_date.split('-')[0]);
+//   });
+// }
+
+// function card(id) {
+//   api.getInfoById(id).then(data => {
+//     // console.log(data);
+//     const markup = buildCartTemplate(data);
+//     // console.log(markup);
+//     insertCardToMain(markup);
+//   });
+// }
+
+// function buildCartTemplate(item) {
+//   return cardTemplate(item);
+// }
+
+// function insertCardToMain(item) {
+//   refs.container.innerHTML = item;
+// }
+
+// function insertCardsToMainPage(items) {
+//   refs.cardList.insertAdjacentHTML('beforeend', items);
+// }
+
+export default user1;
