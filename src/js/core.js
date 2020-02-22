668baabf43018770948497607156a78ed453db6e
IP777 <ivanov_piter@ukr.net>
2020-02-22 16:21:30 +0200

    edits 1

--- src/js/core.js
+++ src/js/core.js
@@ -8,34 +8,16 @@ import refs from './refs';
 import myPnotify from './pnotifyAlerts';
 import proxyElement from './proxyElemen';
 //---------------------------------------------------
-import user1 from './users/user1';
-import user2 from './users/user2';
-import user3 from './users/user3';
+//import user1 from './users/user1';
+//import user2 from './users/user2';
+//import user3 from './users/user3';
 import api from './api';
 
-// console.log(`${imagesLoaded.name} OK`);
-// console.log(`${_.name} OK`);
-// console.log(`${basicLightbox.name} OK`);
-// console.log(imageCardTemplate.name + ' OK');
-refs.test();
-myPnotify.test();
-console.log(`${proxyElement.name} OK`);
-
-user1.test();
+//user1.test();
 // user2.test();
 // user3.test();
 
-<<<<<<< HEAD
-// api.getPopularFilms().then(console.log);
-// api.getInfoById(419704).then(console.log);
-// api.getMoviesByQuery('batman').then(console.log);
-=======
-api.getPopularFilms().then(console.log);
-api.getInfoById(419704).then(console.log);
-
-// api.getMoviesByQuery('batman').then(console.log);
-// api.page = 2;
-// api.getMoviesByQuery('batman').then(console.log);
-// console.log(api.getPageFromLink());
-// console.log(api.getMovieIdFromLink());
->>>>>>> 8e4b177e9c8738d00f5a8aa2fb37bf26690baec6
+refs.serchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  api.getMoviesByQuery(refs.textArea.value).then(console.log);
+});
--- src/js/refs.js
+++ src/js/refs.js
@@ -3,6 +3,8 @@ const refs = {
   intro: document.querySelector('.intro'),
   container: document.querySelector('.container'),
   cardList: document.querySelector('.card__list'),
+  serchForm: document.querySelector('.search-form'),
+  textArea: document.querySelector('#search-form'),
 
   test() {
     console.log(`${Object.getOwnPropertyNames(this)} OK`);
