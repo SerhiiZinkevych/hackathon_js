38a84d1cfed7e58d0968ae4f80d9c5f3d8de407e
Serhii Zinkevych <zinkevych@gmail.com>
2020-02-22 16:14:53 +0200

    SerhiiZinkevych minor changes

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,6 +3,10 @@ const refs = {
   intro: document.querySelector('.intro'),
   container: document.querySelector('.container'),
   cardList: document.querySelector('.card__list'),
+  form: document.querySelector('.search-form'),
+  homeBtn: document.querySelector('#nav-home'),
+  libraryBtn: document.querySelector('#nav-libery'),
+  cards: document.querySelectorAll('.card__link'),
 
   test() {
     console.log(`${Object.getOwnPropertyNames(this)} OK`);
