a568afa3619fc36d11d38fa0b17de32553f21ab2
IP777 <ivanov_piter@ukr.net>
2020-02-23 15:06:46 +0200

    edits 12

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import './styles.css';
 import './css/sidebar.css';
 import './css/footer.css';
 import './css/header.css';
+import './css/searchForm.css';
+import './css/fullCard.css';
 import './js/core';
