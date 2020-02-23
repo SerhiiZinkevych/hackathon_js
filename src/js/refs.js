1ec061c53f8b30d15d26700528f037201ae97b71
Serhii Zinkevych <zinkevych@gmail.com>
2020-02-23 10:30:21 +0200

    buttons watched queue local storage

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,11 +1,13 @@
 const refs = {
   body: document.querySelector('body'),
+  logo: document.querySelector('.site-logo'),
   intro: document.querySelector('.intro'),
   container: document.querySelector('#container'),
   serchForm: document.querySelector('.search-form'),
   textArea: document.querySelector('#search-form'),
   cardContainer: document.querySelector('#container'),
   cardList: document.querySelector('.card_list'),
+  library: document.querySelector('#nav-libery'),
   //itemCard: document.querySelectorAll('.itemCard'),
   // form: document.querySelector('.search-form'),
   // homeBtn: document.querySelector('#nav-home'),
